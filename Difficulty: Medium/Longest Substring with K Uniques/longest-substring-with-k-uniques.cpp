0048894a6eb20cca237fd8211fb7ba49c4f39609
goyalyash82 <goyalyash2310@gmail.com>
2025-06-28 22:52:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -0,0 +1,22 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // your code here
+         unordered_map<char,int> mp;
+        int n = s.size();
+        int ans = -1, j = 0;
+        for(int i = 0; i < n; i++){
+            mp[s[i]]++;
+            if(mp.size() > k){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0)   mp.erase(s[j]);
+                j++;
+            }
+            if(mp.size() == k)  
+                ans = max(ans, i - j + 1);
+        }
+        return ans;
+    }
+};
