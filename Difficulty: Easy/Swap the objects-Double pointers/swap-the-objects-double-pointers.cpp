57542e808c8306d5b1946959a742ef40fb32aa38
goyalyash82 <goyalyash2310@gmail.com>
2025-07-07 00:39:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap the objects-Double pointers/swap-the-objects-double-pointers.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// Function to swap two objects **obj1 and **obj2
+void swapper(Person **obj1, Person **obj2) {
+    // Your code here
+    Person temp=**obj1;
+    **obj1=**obj2;
+    **obj2=temp;
+}
