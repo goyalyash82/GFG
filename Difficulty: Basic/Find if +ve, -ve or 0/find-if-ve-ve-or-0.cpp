62fab58957d83bd95661339ea934a7b209e2a44b
goyalyash82 <goyalyash2310@gmail.com>
2025-07-07 09:12:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+
+void fun(int a) {
+
+    // code here
+        if(a==0){cout<<"Zero";}
+
+    else if(a>0){cout<<"Positive";}
+    else{
+        cout<<"Negative";
+    }
+}
