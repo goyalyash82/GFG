e654224e4a97f9c449290a4ea7e397bb80565be1
goyalyash82 <goyalyash2310@gmail.com>
2025-07-07 08:55:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Digit of a Number/first-digit-of-a-number.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+// Complete the function
+int firstDigit(int n) {
+    // code here
+    while(n>=0){
+        if(n<10){
+            return n;
+        }
+        n=n/10;
+    }
+    
+    
+}
