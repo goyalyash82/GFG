d9064dc7188e205699c3d0b7d958ecdc14126509
goyalyash82 <goyalyash2310@gmail.com>
2025-07-09 13:26:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
@@ -0,0 +1,17 @@
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to check if two arrays are equal or not.
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        // code here
+         sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       if(a.size()!=b.size())
+       return 0;
+       if(a==b)
+       return 1;
+       else
+       return 0;
+    }
+};
