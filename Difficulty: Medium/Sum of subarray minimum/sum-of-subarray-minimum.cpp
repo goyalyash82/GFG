2202bb8b05c41d6148768592cd1b3db4b3b5a588
goyalyash82 <goyalyash2310@gmail.com>
2025-07-09 13:19:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of subarray minimum/sum-of-subarray-minimum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    int sumSubMins(vector<int> &arr) {
+        const int MOD = 1e9 + 7;
+        int n = arr.size();
+        vector<int> ple(n), nle(n);  
+        stack<int> s;
+
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && arr[s.top()] > arr[i]) {
+                s.pop();
+            }
+            ple[i] = s.empty() ? -1 : s.top();
+            s.push(i);
+        }
+
+        while (!s.empty()) s.pop();
+
+        for (int i = n - 1; i >= 0; --i) {
+            while (!s.empty() && arr[s.top()] >= arr[i]) {
+                s.pop();
+            }
+            nle[i] = s.empty() ? n : s.top();
+            s.push(i);
+        }
+
+        long long result = 0;
+        for (int i = 0; i < n; ++i) {
+            long long left = i - ple[i];
+            long long right = nle[i] - i;
+            result = (result + (arr[i] * left % MOD) * right % MOD) % MOD;
+        }
+
+        return (int)result;
+    }
+};
